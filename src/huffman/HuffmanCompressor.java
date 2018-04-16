4a23505cf27296238cfb47dac1d73744eb5b161e
Alejandro Rios <jassoalex97@hotmail.com>
2018-04-16 13:48:26 -0500

    Clase Tree y Serializar árbol generado en HuffmanCompressor

--- src/huffman/HuffmanCompressor.java
+++ src/huffman/HuffmanCompressor.java
@@ -1,52 +1,44 @@
 package huffman;
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.HashMap;
-import java.util.PriorityQueue;
 
 public class HuffmanCompressor {
 
 	public static void main(String[] args) {
-		HashMap<Character, Integer> charFreq = charFreqFromFile(
-				"C:\\Users\\Alejandro\\Documents\\GitHub\\Repositories\\HuffmanCompression\\src\\Harry Potter.txt");
-		PriorityQueue<Node> queue = new PriorityQueue<>();
-		for (int i = 0; i < 256; i++) {
-			if (charFreq.containsKey((char) i))
-				queue.add(new Node((char) i, charFreq.get((char) i)));
+		HashMap<Character, Integer> charFreq = charFreqFromFile("C:\\Users\\Alejandro\\Documents\\GitHub\\Repositories\\HuffmanCompression\\src\\Harry Potter.txt");
+		
+		Tree tree = new Tree(charFreq);
+		tree.print();
+		
+		FileOutputStream fos;
+		FileInputStream fis;
+		
+		try {
+			fos = new FileOutputStream("C:\\Users\\Alejandro\\Documents\\GitHub\\Repositories\\HuffmanCompression\\src\\data.bin");
+			ObjectOutputStream oos = new ObjectOutputStream(fos);
+			oos.writeObject(tree);
+			oos.close();
+			System.out.println(fos.toString());
+			
+			fis = new FileInputStream("C:\\Users\\Alejandro\\Documents\\GitHub\\Repositories\\HuffmanCompression\\src\\data.bin");
+			ObjectInputStream ois = new ObjectInputStream(fis);
+			Tree test = (Tree) ois.readObject();
+			ois.close();
+			System.out.println("\n\n\n\n");
+			test.print();
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-		System.out.println("List of Nodes (char, frequency)");
-		System.out.println("Sorted by char value: ");
-		System.out.println(queue.toString());
-	}
-
-	public static class Node implements Comparable<Node> {
-
-		public char character;
-		public int frequency = 0;
-		public Node left = null;
-		public Node right = null;
-
-		public Node(char character, int frequency) {
-			this.character = character;
-			this.frequency = frequency;
-		}
-
-		@Override
-		public String toString() {
-			if (this.character == '\n')
-				return ("[" + "\\n" + ", " + this.frequency + "]");
-			if (this.character == '\r')
-				return ("[" + "\\r" + ", " + this.frequency + "]");
-			return ("[" + this.character + ", " + this.frequency + "]");
-		}
-
-		@Override
-		public int compareTo(Node node) {
-			return this.frequency - node.frequency;
-		}
-
+		
+		
+		
 	}
 
 	public static HashMap<Character, Integer> charFreqFromFile(String file) {
@@ -69,5 +61,13 @@ public class HuffmanCompressor {
 		}
 		return freqMap;
 	}
+//	
+//	public static byte[] serializeTree(Tree tree) {
+//		byte[] frame = new byte[tree.getNullNodes() + tree.getLeafNodes() * 3];
+//		
+//		
+//		
+//		return frame;
+//	}
 
 }
